7c57273400a9d3a16d2736fa3231e2c4e0a47adc
dndanli <danielkcode@gmail.com>
2021-07-31 19:02:24 -0500

    added sorting of stock prices

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include "scraper.h"
+#include <algorithm>
 #include <jsoncpp/json/json.h>
+#include "scraper.h"
 
+//each vector stores a piece of information of a stock
 std::vector<Json::Value> stockPrice;
 std::vector<Json::Value> companyName;
 std::vector<Json::Value> marketCap;
@@ -14,6 +16,7 @@ std::vector<Json::Value> totalCash;
 std::vector<Json::Value> forwardPE;
 std::vector<Json::Value> beta;
 
+//allows user to save stock information to a csv file
 void saveDataToFile(std::vector<Json::Value> formattedDataSource)
 {
   std::fstream csvFile;
@@ -36,52 +39,71 @@ int main()
   Json::Value jsonData;
   std::string error;
 
-  std::vector<std::string> stockVec;
+  std::string userChoice;
+  //stores stock tickers
+  std::vector<std::string> stockTickers;
+
+  //stores ticker prices as strings, used to remove commas from json value before converting to a double.
+  std::vector<std::string> strTickerPrices;
 
   std::string ticker;
-  int stockQuantity;
-  int count = 0;
+  int tickerCount;
+  int count;
 
-  std::cout << "How any stocks would you like to fetch?" << std::endl;
-  std::cin >> stockQuantity;
+  //prompting user
+  std::cout << "How any tickers would you like to fetch?" << std::endl;
+  std::cin >> tickerCount;
 
-  std::cout << "Stock name" << std::endl;
+  std::cout << "Enter a ticker" << std::endl;
 
-  while (count < stockQuantity)
+  //prompting for ticker names
+  for (count = 0; count < tickerCount; count++)
   {
     std::cin >> ticker;
-    stockVec.push_back(ticker);
-    count++;
+    stockTickers.push_back(ticker);
   }
 
   std::cout << std::endl;
-  for (int i = 0; i < stockVec.size(); i++)
+
+  //scraping all the data
+  for (int i = 0; i < stockTickers.size(); i++)
   {
+    //initializing scraper instance for current ticker
+    Scraper s(stockTickers[i]);
 
-    Scraper s(stockVec[i]);
-    std::string data = s.fetchHtml(s.url);
-    s.formatData(data);
+    //fetching html
+    std::string htmlData = s.fetchHtml(s.url);
 
-    bool isParsingSuccessful = reader->parse(data.c_str(), data.c_str() + data.size(), &jsonData, &error);
+    //format to json
+    s.formatData(htmlData);
+
+    //parsing to  valid json
+    bool isParsingSuccessful = reader->parse(htmlData.c_str(), htmlData.c_str() + htmlData.size(), &jsonData, &error);
 
     if (!isParsingSuccessful)
     {
-      std::cout << data << std::endl;
       std::cout << error << std::endl;
     }
 
+    //querying data from json
+
+    // --- Company Metadata -------------
+    strTickerPrices.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["price"]["regularMarketPrice"]["fmt"].asCString());
     stockPrice.push_back((jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["price"]["regularMarketPrice"]["fmt"]));
-    companyName.push_back(jsonData["context"]["dispatcher"]["stores"]["StreamDataStore"]["quoteData"][stockVec[i]]["longName"]);
-    marketCap.push_back(jsonData["context"]["dispatcher"]["stores"]["StreamDataStore"]["quoteData"][stockVec[i]]["marketCap"]["fmt"]);
+    companyName.push_back(jsonData["context"]["dispatcher"]["stores"]["StreamDataStore"]["quoteData"][stockTickers[i]]["longName"]);
+    marketCap.push_back(jsonData["context"]["dispatcher"]["stores"]["StreamDataStore"]["quoteData"][stockTickers[i]]["marketCap"]["fmt"]);
     trailingPE.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["summaryDetail"]["trailingPE"]["fmt"]);
 
+    // --- Enterprice Value per share ---
     enterpriseValue.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["defaultKeyStatistics"]["enterpriseValue"]["fmt"]);
     totalCash.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["financialData"]["totalCash"]["fmt"]);
     totalDebt.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["financialData"]["totalDebt"]["fmt"]);
 
+    // --- Assumptions Metadata ---------
     forwardPE.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["defaultKeyStatistics"]["forwardPE"]["fmt"]);
     beta.push_back(jsonData["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["defaultKeyStatistics"]["beta"]["fmt"]);
 
+    //displaying info
     std::cout << "+----------------------------------------------+" << std::endl;
     std::cout << "Company Metadata" << std::endl;
     std::cout << std::endl;
@@ -108,6 +130,7 @@ int main()
     std::cout << "+----------------------------------------------+" << std::endl;
   }
 
+  // saving data to csv file (optional)
   saveDataToFile(companyName);
   saveDataToFile(stockPrice);
   saveDataToFile(marketCap);
@@ -117,6 +140,33 @@ int main()
   saveDataToFile(totalDebt);
   saveDataToFile(beta);
   saveDataToFile(forwardPE);
+
+  std::cout << "Would you like to see stocks with the lowest prices?(y/n)" << std::endl;
+  std::cin >> userChoice;
+  if (userChoice == "y")
+  {
+    //remove comma to not affect sorting the price values
+    for (size_t i = 0; i < strTickerPrices.size(); i++)
+    {
+      strTickerPrices[i].erase(std::remove(strTickerPrices[i].begin(), strTickerPrices[i].end(), ','), strTickerPrices[i].end());
+    }
+
+    //converting vector of strings into vector of double
+    std::vector<double> tickerPrices(strTickerPrices.size());
+    transform(strTickerPrices.begin(), strTickerPrices.end(), tickerPrices.begin(),
+              [](std::string const &val)
+              { return stod(val); });
+
+    //finally, sort
+    std::sort(tickerPrices.begin(), tickerPrices.end(), std::less<>());
+
+    //displaying
+    for (auto price : tickerPrices)
+    {
+      std::cout << price << std::endl;
+    }
+  }
+  std::cout << "Goodbye!" << std::endl;
 
   return 0;
 }
