fb6f256934af681373e061a8300df6290862a669
dndanli <danielkcode@gmail.com>
2021-08-04 22:25:25 -0500

    changed saveData declaration

--- utilityFunctions.h
+++ utilityFunctions.h
@@ -2,5 +2,5 @@
 #include <jsoncpp/json/json.h>
 #include <fstream>
 
-void saveDataToFile(std::vector<Json::Value> formattedDataSource);
+void saveDataToFile(std::vector<Json::Value> formattedDataSource, std::string rowName);
 void printLowestPrices(std::vector<std::string> tickerName, std::vector<double> tickerPrices);
